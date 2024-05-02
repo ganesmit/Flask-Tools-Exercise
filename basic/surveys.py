3cd08d92e266b63c46ec7b7c72d853b62fcb261b
ganesmit <antionenesmith@gmail.com>
2024-05-02 10:37:14 -0400

    Initial Commit

---
+++ basic/surveys.py
@@ -0,0 +1,53 @@
+class Question:
+    """Question on a questionnaire."""
+
+    def __init__(self, question, choices=None, allow_text=False):
+        """Create question (assume Yes/No for choices."""
+
+        if not choices:
+            choices = ["Yes", "No"]
+
+        self.question = question
+        self.choices = choices
+        self.allow_text = allow_text
+
+
+class Survey:
+    """Questionnaire."""
+
+    def __init__(self, title, instructions, questions):
+        """Create questionnaire."""
+
+        self.title = title
+        self.instructions = instructions
+        self.questions = questions
+
+
+satisfaction_survey = Survey(
+    "Customer Satisfaction Survey",
+    "Please fill out a survey about your experience with us.",
+    [
+        Question("Did you find everything you were looking for today?"),
+        Question("Did someone assist you in locating what you were looking for?"),
+        Question("On average, how much do you spend each year on groceries?",
+                 ["Less than $10,000", "$10,000 or more"]),
+        Question("Are you likely to recommend us?"),
+    ])
+
+personality_quiz = Survey(
+    "Personality Test",
+    "Learn more about yourself with our personality quiz!",
+    [
+        Question("Do you like to read newspapers or books?",
+                 ["newspapers", "books"]),
+        Question("Do you ever take naps during the day?"),
+        Question("Do you exercise in the morning?"),
+        Question("What is your favorite hobby?",
+                 allow_text=True),
+    ]
+)
+
+surveys = {
+    "satisfaction": satisfaction_survey,
+    "personality": personality_quiz,
+}
